5d7d195013916b9e098302db1720076de2459500
girvel <widauka@ya.ru>
2023-08-26 21:14:00 +0500

    Window & rebalance of armor

--- src/entities/generic/window.py
+++ src/entities/generic/window.py
@@ -3,9 +3,9 @@ from ecs import OwnedEntity
 from src.systems.acting.attack import Health, ArmorKind
 
 
-class Bed(OwnedEntity):
+class Window(OwnedEntity):
     name = 'bed'
     character = '='
 
     def __init__(self):
-        self.health = Health(10, ArmorKind.Wood)
+        self.health = Health(10, ArmorKind.Glass)
