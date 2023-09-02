6fd639e647fb9b1e90edf9599a22ba0335b5a3a8
girvel <widauka@ya.ru>
2023-09-03 01:46:10 +0500

    Pretty logs (closes #14)

--- fallen.py
+++ fallen.py
@@ -9,6 +9,24 @@ from src import init_ecs
 
 log = logging.getLogger(__name__)
 
+class PrettyFormatter(logging.Formatter):
+    short_levels = {
+        "DEBUG": "DEBUG",
+        "INFO": "INFO ",
+        "WARNING": "WARN ",
+        "ERROR": "ERROR",
+        "CRITICAL": "FATAL",
+    }
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.levelname = self.short_levels[record.levelname]
+        record.pathname = str(Path(record.pathname).relative_to(Path(__file__).parent))
+        return super().format(record)
+        # return (
+        #     f"[{self.short_levels[record.levelname]} {record.asctime}] {record.filename}:{record.lineno} "
+        #     f"{record.message}"
+        # )
+
 
 def main(track_file: Optional[str] = None, debug_mode: bool = False):
     """
@@ -18,7 +36,14 @@ def main(track_file: Optional[str] = None, debug_mode: bool = False):
         track_file: file to redirect input from
         debug_mode: whether to enable debug console
     """
-    logging.basicConfig(filename='.last.log', encoding='utf-8', level=logging.DEBUG)
+    file_handler = logging.FileHandler('.last.log')
+    file_handler.setFormatter(PrettyFormatter(
+        fmt="[%(levelname)s %(asctime)s]  %(pathname)s:%(lineno)d  %(message)s",
+        datefmt="%H:%M:%S",
+    ))
+
+    logging.basicConfig(handlers=[file_handler], encoding='utf-8', level=logging.DEBUG)
+
     log.info("Started")
 
     logging.getLogger('numba').setLevel(logging.WARNING)
