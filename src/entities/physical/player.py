4213635ffe10b2e89a50e4013e4bc78239e5726e
girvel <widauka@ya.ru>
2023-09-03 11:25:29 +0500

    Strange name generation for player character (closes #13)

--- src/entities/physical/player.py
+++ src/entities/physical/player.py
@@ -1,12 +1,16 @@
+import random
+from pathlib import Path
+
+import yaml
 from ecs import OwnedEntity
 
 from src.lib.vector import map_grid
 from src.systems.acting.damage import DamageKind, ArmorKind, Weapon, Health
 from src.systems.ai import Kind, Senses
 
+names = yaml.safe_load(Path("assets/strange_names.yaml").read_text())
 
 class Player(OwnedEntity):
-    name = 'Sir Aethan'
     character = '@'
 
     senses = Senses(24, 40, 1)
@@ -14,6 +18,7 @@ class Player(OwnedEntity):
     act = None
 
     def __init__(self):
+        self.name = "Sir " + " ".join(random.choice(collection) for collection in names)
         self.weapon = Weapon(15, DamageKind.Slashing)
         self.health = Health(100, ArmorKind.Steel)
         self.classifiers = {Kind.Animate}
