0c5314f6efd643f87c8a34c764a896ba620aa7c8
girvel <widauka@ya.ru>
2023-09-11 17:10:25 +0300

    Make Attack action destruction-safe

--- src/engine/acting/actions/attack.py
+++ src/engine/acting/actions/attack.py
@@ -1,9 +1,11 @@
 from collections import namedtuple
 
+from ecs import exists
+
 from src.engine.acting.damage import inflict_damage
 
 
 class Attack(namedtuple("AttackBase", "target")):
     def execute(self, actor, level, hades, genesis):
-        if self.target is None or "health" not in self.target: return
+        if self.target is None or not exists(self.target) or "health" not in self.target: return
         inflict_damage(self.target, actor.weapon, hades)
