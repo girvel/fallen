c84b424abe71c407e6eb881752be24a49eb120c1
girvel <widauka@ya.ru>
2023-09-23 11:10:59 +0300

    Catching  logging errors

--- fallen.py
+++ fallen.py
@@ -1,3 +1,5 @@
+"""CLI and launch script for Fallen"""
+
 import curses
 import logging
 from pathlib import Path
@@ -6,25 +8,7 @@ from typing import Optional
 import fire
 
 from src import init_ecs
-
-
-class PrettyFormatter(logging.Formatter):
-    short_levels = {
-        "DEBUG": "DEBUG",
-        "INFO": "INFO ",
-        "WARNING": "WARN ",
-        "ERROR": "ERROR",
-        "CRITICAL": "FATAL",
-    }
-
-    def format(self, record: logging.LogRecord) -> str:
-        record.levelname = self.short_levels[record.levelname]
-        record.pathname = str(Path(record.pathname).relative_to(Path(__file__).parent))
-        return super().format(record)
-        # return (
-        #     f"[{self.short_levels[record.levelname]} {record.asctime}] {record.filename}:{record.lineno} "
-        #     f"{record.message}"
-        # )
+from src.init_logging import init_logging
 
 
 def main(track_file: Optional[str] = None, debug_mode: bool = False):
@@ -35,17 +19,8 @@ def main(track_file: Optional[str] = None, debug_mode: bool = False):
         track_file: file to redirect input from
         debug_mode: whether to enable debug console
     """
-    file_handler = logging.FileHandler('.last.log')
-    file_handler.setFormatter(PrettyFormatter(
-        fmt="[%(levelname)s %(asctime)s]  %(pathname)s:%(lineno)d  %(message)s",
-        datefmt="%H:%M:%S",
-    ))
-
-    logging.basicConfig(handlers=[file_handler], encoding='utf-8', level=logging.DEBUG)
-
+    init_logging()
     logging.info("Started")
-
-    logging.getLogger('numba').setLevel(logging.WARNING)
 
     curses.wrapper(init_ecs.init,
         track=track_file and Path(track_file).read_text().replace("\n", ""),
---
+++ src/init_logging.py
@@ -0,0 +1,57 @@
+import logging
+import sys
+from pathlib import Path
+
+root_directory = Path(__file__).parent.parent
+
+class PrettyFormatter(logging.Formatter):
+    short_levels = {
+        "DEBUG": "DEBUG",
+        "INFO": "INFO ",
+        "WARNING": "WARN ",
+        "ERROR": "ERROR",
+        "CRITICAL": "FATAL",
+    }
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.levelname = self.short_levels[record.levelname]
+        record.pathname = str(Path(record.pathname).relative_to(root_directory))
+        return super().format(record)
+        # return (
+        #     f"[{self.short_levels[record.levelname]} {record.asctime}] {record.filename}:{record.lineno} "
+        #     f"{record.message}"
+        # )
+
+
+class SafeFileHandler(logging.FileHandler):
+    def handleError(self, record: logging.LogRecord) -> None:
+        ex_type, ex, traceback = sys.exc_info()
+        try:
+            logging.error("Exception while logging:")
+            logging.exception(ex)
+        except Exception as ex2:
+            try:
+                message = (
+                    f"Exception while handling exception while logging: {ex2}\n\n"
+                    f"Exception while logging: {ex}\n{traceback}"
+                )
+            except:
+                message = "Conversion error: Exception -> str"
+
+            try:
+                (root_directory / ".backup.log").write_text(message)
+            except Exception as ex3:
+                sys.stderr.write(f"3rd degree logging exception: {ex3}\n\n" + message)
+
+
+def init_logging():
+    handler = SafeFileHandler('.last.log')
+    handler.setFormatter(PrettyFormatter(
+        fmt="[%(levelname)s %(asctime)s]  %(pathname)s:%(lineno)d  %(message)s",
+        datefmt="%H:%M:%S",
+    ))
+
+    logging.basicConfig(handlers=[handler], encoding='utf-8', level=logging.DEBUG)
+
+    logging.getLogger('numba').setLevel(logging.WARNING)
+    logging.info("Initialized logging")
