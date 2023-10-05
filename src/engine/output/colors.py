c83f770ad48ffd2a4693dfb8371fd743229a4319
girvel <widauka@ya.ru>
2023-10-05 15:00:11 +0300

    Fix indents in colors.py

--- src/engine/output/colors.py
+++ src/engine/output/colors.py
@@ -13,14 +13,14 @@ cyan = curses.COLOR_CYAN
 magenta = curses.COLOR_MAGENTA
 
 _color_scheme = {
-  black:   0x1d1f21,
-  red:     0xcc6666,
-  green:   0x93bd68,
-  yellow:  0xf0c674,
-  blue:    0x81a2be,
-  magenta: 0xb294bb,
-  cyan:    0x8abeb7,
-  white:   0xc5c8c6,
+    black:   0x1d1f21,
+    red:     0xcc6666,
+    green:   0x93bd68,
+    yellow:  0xf0c674,
+    blue:    0x81a2be,
+    magenta: 0xb294bb,
+    cyan:    0x8abeb7,
+    white:   0xc5c8c6,
 }
 
 @dataclass
