78a59228d03310abee31740e33d7f774f097a219
girvel <widauka@ya.ru>
2023-10-05 14:57:05 +0300

    Custom color scheme

--- src/engine/output/colors.py
+++ src/engine/output/colors.py
@@ -12,14 +12,35 @@ yellow = curses.COLOR_YELLOW
 cyan = curses.COLOR_CYAN
 magenta = curses.COLOR_MAGENTA
 
+_color_scheme = {
+  black:   0x1d1f21,
+  red:     0xcc6666,
+  green:   0x93bd68,
+  yellow:  0xf0c674,
+  blue:    0x81a2be,
+  magenta: 0xb294bb,
+  cyan:    0x8abeb7,
+  white:   0xc5c8c6,
+}
+
 @dataclass
 class ColorPair:
-    fg: int = curses.COLOR_WHITE
-    bg: int = curses.COLOR_BLACK
+    fg: int = white
+    bg: int = black
 
     @classmethod
     def initialize(cls):
         print('\033[?1003h')  # magical value to enable colors
+
+        if curses.can_change_color():
+            for color_i in range(8):
+                hex = _color_scheme[color_i]
+                curses.init_color(color_i,
+                    round((hex // 0x010000        ) / 0x100 * 1000),
+                    round((hex // 0x000100 % 0x100) / 0x100 * 1000),
+                    round((hex             % 0x100) / 0x100 * 1000),
+                )
+
         for fg_i in range(8):
             for bg_i in range(8):
                 if fg_i == 0 and bg_i == 0: continue
