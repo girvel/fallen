f34666316fafdeb70cf1e2185a0785aa8496858d
girvel <widauka@ya.ru>
2023-11-16 14:26:26 +0500

    Reimplement color tags, <li>

--- src/engine/output/html.py
+++ src/engine/output/html.py
@@ -1,31 +1,32 @@
 import re
+from enum import Enum
 from html.parser import HTMLParser
 
 from src.engine.output.colors import ColorPair, yellow, white, red
 from src.engine.output.grid_rendering import put_string_on_grid
 from src.lib.toolkit import add_multiline_string
-from src.lib.vector import create_grid
+from src.lib.vector import create_grid, grid_size
 
 
-# class HorizontalAlignment(Enum):
-#     left = 0
-#     center = 1
-#     right = 2
-#
-#
-# class VerticalAlignment(Enum):
-#     top = 0
-#     center = 1
-#     bottom = 2
+class HorizontalAlignment(Enum):
+    left = 0
+    center = 1
+    right = 2
+
+
+class VerticalAlignment(Enum):
+    top = 0
+    center = 1
+    bottom = 2
 
 
 class CursesHtmlRenderer(HTMLParser):
     size = None
     y = None
     x = None
-    # color_stack = [ColorPair()]
-    # horizontal_alignment = HorizontalAlignment.left
-    # vertical_alignment = VerticalAlignment.top
+    color_stack = [ColorPair()]
+    horizontal_alignment = HorizontalAlignment.left
+    vertical_alignment = VerticalAlignment.top
 
     def render(self, size, html, **kwargs):
         self.grid = create_grid(size, lambda: " ")
@@ -33,32 +34,25 @@ class CursesHtmlRenderer(HTMLParser):
         self.feed(re.sub(r"\n\s*", "", html))
         return self.grid
 
-    # def handle_starttag(self, tag, attrs, **kwargs):
-    #     attrs = dict(attrs)
-    #
-    #     match tag:
-    #         case "center":
-    #             self.horizontal_alignment = HorizontalAlignment.center
-    #         case "right":
-    #             self.horizontal_alignment = HorizontalAlignment.right
-    #         case "bottom":
-    #             self.vertical_alignment = VerticalAlignment.bottom
-    #             self.y = self.window.getmaxyx()[0] - 1
-    #         case "y":
-    #             self.color_stack.append(ColorPair(yellow))
-    #         case "rw":
-    #             self.color_stack.append(ColorPair(white, red))
-    #         case "li":
-    #             h, w = self.window.getmaxyx()
-    #             self.y, self.x = add_multiline_string(
-    #                 self.window,
-    #                 self.y, self.x,
-    #                 0, 0,
-    #                 h, w,
-    #                 "- ", ColorPair(yellow),
-    #             )
-    #         case "color":
-    #             self.color_stack.append(ColorPair.from_code(int(attrs["code"])))
+    def handle_starttag(self, tag, attrs, **kwargs):
+        attrs = dict(attrs)
+
+        match tag:
+            # case "center":
+            #     self.horizontal_alignment = HorizontalAlignment.center
+            # case "right":
+            #     self.horizontal_alignment = HorizontalAlignment.right
+            # case "bottom":
+            #     self.vertical_alignment = VerticalAlignment.bottom
+            #     self.cursor_p = (0, grid_size(self.grid)[1] - 1)
+            case "y":
+                self.color_stack.append(ColorPair(yellow))
+            case "rw":
+                self.color_stack.append(ColorPair(white, red))
+            case "li":
+                self.cursor_p = put_string_on_grid(self.grid, self.cursor_p, "- ", ColorPair(yellow).to_curses())
+            case "color":
+                self.color_stack.append(ColorPair.from_code(int(attrs["code"])))
 
     def handle_endtag(self, tag):
         match tag:
@@ -67,14 +61,14 @@ class CursesHtmlRenderer(HTMLParser):
             # case "bottom":
             #     self.vertical_alignment = VerticalAlignment.top
             #     self.y = 0
-            # case "y" | "rw" | "color":
-            #     self.color_stack.pop()
+            case "y" | "rw" | "color":
+                self.color_stack.pop()
             case "div" | "p" | "li" | "br":
                 _, y = self.cursor_p
                 self.cursor_p = (0, y + 1)
 
     def handle_data(self, data):
-        self.cursor_p = put_string_on_grid(self.grid, self.cursor_p, data, ColorPair(white).to_curses())
+        self.cursor_p = put_string_on_grid(self.grid, self.cursor_p, data, self.color_stack[-1].to_curses())
 
         # h, w = self.window.getmaxyx()
         #
