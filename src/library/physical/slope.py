3120f7b5512b63746d8bbf5f6903bcb3a08401ed
girvel <widauka@ya.ru>
2023-12-12 21:17:43 +0500

    Slope entity

--- src/library/physical/slope.py
+++ src/library/physical/slope.py
@@ -3,25 +3,24 @@ import random
 from src.engine.acting import armor_kind
 from src.engine.acting.damage import Health
 from src.engine.language.name import Name
-from src.engine.output.colors import ColorPair, yellow
+from src.engine.output.colors import ColorPair
 from src.library.abstract.material import Material
 from src.library.special.genesis import Genesis
 from src.library.special.hades import Hades
 from src.library.tiles.ruins import Ruins
 
 
-class ThickWall(Material):
-    name = Name.auto("стена")
-    character = '#'
-    color = ColorPair(yellow)
-    solid_flag = None
+class Slope(Material):
+    name = Name.auto("склон")
+    character = "%"
+    color = ColorPair()
     layer = "physical"
 
     boring_flag = None
 
     def __post_init__(self):
-        self.health = Health(random.randrange(5000, 10001, 500), armor_kind.stone)
+        self.health = Health(random.randrange(2500, 6000), armor_kind.stone)
 
-    def on_death(_, self, _hades: Hades, genesis: Genesis):  # TODO review this pattern. Can I do better?
+    def on_death(_, self, _hades: Hades, genesis: Genesis):
         genesis.entities_to_create.add(Ruins(p=self.p, level=self.level))
         return True
