e9263b7dc224a564a386a3cc9f30efcf69798ebe
girvel <widauka@ya.ru>
2023-12-27 08:40:45 +0500

    More colors for flower

--- src/assets/tiles/flower.py
+++ src/assets/tiles/flower.py
@@ -3,7 +3,7 @@ import random
 from src.engine.acting.damage import Health
 from src.engine.acting import armor_kind
 from src.engine.language.name import Name
-from src.engine.output.colors import ColorPair, cyan, red, magenta
+from src.engine.output.colors import ColorPair, cyan, red, magenta, yellow, blue, white
 from src.assets.abstract.material import Material
 
 
@@ -17,4 +17,4 @@ class Flower(Material):
         self.health = Health(1, armor_kind.none)
 
         if not hasattr(self, "color"):  # for subclassing
-            self.color = random.choice([ColorPair(cyan), ColorPair(red), ColorPair(magenta)])
+            self.color = ColorPair(random.choice([cyan, red, magenta, blue, yellow]))
