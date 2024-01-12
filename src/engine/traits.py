3893f5f174f1b782daa13100ce68cb4da9d97fb6
girvel <widauka@ya.ru>
2024-01-12 14:59:32 +0500

    Edit the dungeon in 01 level a bit

--- src/engine/traits.py
+++ src/engine/traits.py
@@ -1,7 +1,10 @@
 class Traits:
-    naivety = 0  # 0..2 (2)
-    pain = 0  # 0..2 (3)
-    curiosity = 0  # 0..1 (4)
+    # TODO redo to:
+    # fun, dignity, passion, exploration, prosperity, ambition, vengeance
+
+    naivety = 0  # 0..2
+    pain = 0  # 0..2
+    curiosity = 0  # 0..1
     brutality = 0  # 0..1
 
     bravery = 0  # 0..0
