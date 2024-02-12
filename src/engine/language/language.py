c1afdbcf18892801c1124abcb35ecd9738765922
girvel <widauka@ya.ru>
2024-02-12 09:28:19 +0500

    Fix sign (closes #341)

--- src/engine/language/language.py
+++ src/engine/language/language.py
@@ -22,22 +22,23 @@ class In:
         return f"в {self.area.name:пр}"
 
 
+@dataclass
 class Around:
     def __str__(self):
         return "неподалёку"
 
 
-def placement(markup: Markup, p: int2) -> Near | In:
+def placement(markup: Markup, p: int2) -> Near | In | Around:
     for house in markup.houses:
         if ge2(p, house.house_borders[0]) and lt2(p, house.house_borders[1]):
             return In(house)
 
     areas = (
-        [(zone, zone.center) for zone in markup.zones] +
-        [(house, house.house_borders[0]) for house in markup.houses] +
-        [(house, house.house_borders[1]) for house in markup.houses]
+        [(zone, zone.center, 2) for zone in markup.zones] +
+        [(house, house.house_borders[0], 1) for house in markup.houses] +
+        [(house, house.house_borders[1], 1) for house in markup.houses]
     )
 
     if len(areas) == 0: return Around()
 
-    return Near(min(areas, key=lambda a: d2(a[1], p))[0])
+    return Near(min(areas, key=lambda a: d2(a[1], p) / a[2])[0])
