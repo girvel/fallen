7c93ca2464d1ed6db30acf7ded72e6ed6cde7bfb
girvel <widauka@ya.ru>
2024-04-27 15:03:13 +0500

    Main character w/ correct font

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
+local main_font
+
+love.load = function()
+  main_font = love.graphics.newFont("fonts/BigBlueTerm437NerdFontMono-Regular.ttf", 24)
+end
+
 love.draw = function()
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.print("@", main_font, 400, 300)
 end
