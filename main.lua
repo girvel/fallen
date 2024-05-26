e9453144d8079aea710b52802f37926eea00b5d3
girvel <widauka@ya.ru>
2024-05-27 02:15:35 +0500

    Movement on any key

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+-- TODO fixed size cells, vectors
+-- TODO basic WASD movement
+
 local tiny = require("lib.tiny")
 
 local world = tiny.world()
@@ -6,9 +9,17 @@ love.load = function()
   local main_font = love.graphics.newFont("fonts/BigBlueTerm437NerdFontMono-Regular.ttf", 24)
 	world:add(tiny.processingSystem({
 		filter = tiny.requireAll("position", "sprite"),
-    is_drawing = true,
+    base_callback = "draw",
 		process = function(_, entity)
 			love.graphics.print(entity.sprite.character, main_font, entity.position[1], entity.position[2])
+		end,
+	}))
+
+	world:add(tiny.processingSystem({
+		filter = tiny.requireAll("player_flag"),
+    base_callback = "keypressed",
+		process = function(_, entity)
+      entity.position[1] = entity.position[1] + 20
 		end,
 	}))
 
@@ -17,6 +28,7 @@ love.load = function()
     sprite = {
       character = "@",
     },
+    player_flag = true,
   })
 
   world:add({
@@ -41,6 +53,9 @@ love.load = function()
   })
 end
 
-love.draw = function()
-  world:update(nil, function(_, entity) return entity.is_drawing end)
+for _, callback_name in ipairs({"draw", "keypressed"}) do
+  love[callback_name] = function() -- TODO pass varargs downwards
+    world:update(nil, function(_, entity) return entity.base_callback == callback_name end)
+  end
 end
+
