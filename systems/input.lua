3a93a65562f9e813a3f02a047374eb0a8bf5cb07
girvel <widauka@ya.ru>
2024-05-29 15:42:00 +0500

    Refactor input

--- systems/input.lua
+++ systems/input.lua
@@ -1,25 +1,27 @@
-local movement_hotkeys = {
-  w = Vector({0, -1}),
-  a = Vector({-1, 0}),
-  s = Vector({0, 1}),
-  d = Vector({1, 0}),
+local move = function(direction)
+  return function(entity)
+    if entity.turn_resources.movement <= 0 then return end
+    entity.position = entity.position + direction
+    entity.turn_resources.movement = entity.turn_resources.movement - 1
+  end
+end
+
+local hotkeys = {
+  w = move(Vector({0, -1})),
+  a = move(Vector({-1, 0})),
+  s = move(Vector({0, 1})),
+  d = move(Vector({1, 0})),
+
+  space = function(entity)
+    entity.turn_resources.movement = entity.turn_resources.movement_max
+  end,
 }
-local turn_skip_hotkey = "space"
 
 return Tiny.processingSystem({
   filter = Tiny.requireAll("player_flag"),
   base_callback = "keypressed",
   process = function(_, entity, _, event)
-    local _, scancode = unpack(event)
-
-    if scancode == turn_skip_hotkey then
-      entity.turn_resources.movement = entity.turn_resources.movement_max
-      return
-    end
-
-    local movement = movement_hotkeys[scancode]
-    if movement == nil or entity.turn_resources.movement <= 0 then return end
-    entity.position = entity.position + movement
-    entity.turn_resources.movement = entity.turn_resources.movement - 1
+    local action = hotkeys[event[2]]
+    if action ~= nil then return action(entity) end
   end,
 })
