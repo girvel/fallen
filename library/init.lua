542a44eefbe960ca2a93de3c86d8ae3b54540983
girvel <widauka@ya.ru>
2024-06-01 10:11:34 +0500

    Autoload animation pack by folder

--- library/init.lua
+++ library/init.lua
@@ -114,6 +114,25 @@ local hotkeys = {
   end,
 }
 
+local load_animation_pack = function(folder_path)
+  local result = {}
+  for _, file_name in ipairs(love.filesystem.getDirectoryItems(folder_path)) do
+    local i = file_name:find(".png$")
+    local frame_number = tonumber(file_name:sub(i - 2, i - 1))
+    local animation_name
+    if frame_number then
+      animation_name = file_name:sub(0, i - 4)
+    else
+      frame_number = 1
+      animation_name = file_name:sub(0, i - 1)
+    end
+
+    if not result[animation_name] then result[animation_name] = {} end
+    result[animation_name][frame_number] = love.graphics.newImage(folder_path .. "/" .. file_name)
+  end
+  return result
+end
+
 local player_character_pack = {
   idle_up = {
     love.graphics.newImage("assets/sprites/player_character/idle_up.png"),
@@ -138,6 +157,8 @@ local player_character_pack = {
     love.graphics.newImage("assets/sprites/player_character/move_down_03.png"),
   },
 }
+
+local player_character_pack = load_animation_pack("assets/sprites/player_character")
 
 module.player = function()
   return common.extend(module.creature(), {
