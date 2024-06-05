dabbedefb2e85938b1d3b28479e623efd2589c4f
girvel <widauka@ya.ru>
2024-06-05 10:41:57 +0500

    Grid-based display system

--- systems/display_scene.lua
+++ systems/display_scene.lua
@@ -1,12 +1,26 @@
+local stateful = require("tech.stateful")
+
+
 local CELL_DISPLAY_SIZE = 16
 local default_font = love.graphics.newFont("assets/fonts/joystix.monospace-regular.otf", 7)
 
-return Tiny.processingSystem({
-  filter = function(_, e) return e.sprite and (e.position or e.off_grid_position) end,
+return Tiny.system({
+  filter = Tiny.requireAll("sprite", "position"),
   base_callback = "draw",
 
-  preProcess = function()
-    -- love.graphics.setBackgroundColor(utils.hex_color("332d58"))
+  update = function(self, state, event)
+    local grid_size = state.grids.solids.size
+    for _, l in ipairs(stateful.GRID_LAYERS) do
+      for x = 1, grid_size[1] do
+        for y = 1, grid_size[2] do
+          local entity = state.grids[l][Vector({x, y})]
+
+          if entity and self:filter(entity) then
+            self:process(entity, state, event)
+          end
+        end
+      end
+    end
   end,
 
   process = function(_, entity, state)
@@ -28,12 +42,11 @@ return Tiny.processingSystem({
 
     if not entity.sprite.image then return end
 
-    love.graphics.push()
-    love.graphics.scale(2)
+    love.graphics.applyTransform(state.transform)
 
     local scaled_position = ((entity.position - Vector({1, 1})) * CELL_DISPLAY_SIZE):ceil()
     love.graphics.draw(entity.sprite.image, unpack(scaled_position))
 
-    love.graphics.pop()
+    love.graphics.replaceTransform(love.math.newTransform())
   end,
 })
--- tech/stateful.lua
+++ tech/stateful.lua
@@ -5,12 +5,16 @@ local module = {}
 local module_mt = {}
 setmetatable(module, module_mt)
 
-local GRID_LAYERS = {"tiles", "solids", "sfx"}
+module.GRID_LAYERS = {"tiles", "solids", "sfx"}
 
 module_mt.__call = function()
+  local transform = love.math.newTransform()
+  transform:scale(2)
+
 	return {
     -- grids
     world = Tiny.world(unpack(require("systems"))),
+    transform = transform,
 
     add = function(self, entity)
       self.world:add(entity)
@@ -34,11 +38,11 @@ module_mt.__call = function()
 
     load_level = function(self, path, palette)
       local level_lines = love.filesystem.read(path):split("\n")
-      self.grids = Fun.iter(GRID_LAYERS)
+      self.grids = Fun.iter(module.GRID_LAYERS)
         :map(function(layer) return layer, Grid(Vector({#level_lines[1], #level_lines})) end)
         :tomap()
 
-      for _, layer in ipairs(GRID_LAYERS) do
+      for _, layer in ipairs(module.GRID_LAYERS) do
         for y, line in ipairs(level_lines) do
           for _, x, character in Fun.iter(line):enumerate() do
             local factory = (palette[layer] or {})[character]
