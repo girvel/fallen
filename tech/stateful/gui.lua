a38d4b61794ecb052246f9a77e31ee068268cad6
Nikita Dobrynin <widauka@ya.ru>
2024-06-28 19:33:51 +0500

    convert_line_breaks()

--- tech/stateful/gui.lua
+++ tech/stateful/gui.lua
@@ -24,12 +24,40 @@ local parse_markdown = function(content)
   return result
 end
 
+local convert_line_breaks = function(token_list)
+  local result = {{}}
+
+  for _, token in ipairs(token_list) do
+    local content = token.content
+    while true do
+      local i = content:find("\n")
+      if not i then break end
+      if i > 1 then
+        table.insert(result[#result], {
+          content = content:sub(1, i - 1),
+          link = token.link,
+        })
+      end
+      table.insert(result, {})
+      content = content:sub(i + 1)
+    end
+    if #content > 0 then
+      table.insert(result[#result], {
+        content = content,
+        link = token.link,
+      })
+    end
+  end
+
+  return result
+end
+
 return {
   font = love.graphics.newFont("assets/fonts/joystix.monospace-regular.otf", 12),
 
   show_page = function(self, path)
     local content = love.filesystem.read(path)
-    Log.trace(parse_markdown(content))
+    Log.trace(convert_line_breaks(parse_markdown(content)))
     -- self.text_entities = Fun.iter(parse_markdown(content))
     --   :map(function(e) return State:add(e) end)
     --   :totable()
