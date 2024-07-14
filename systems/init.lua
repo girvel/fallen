7b5c3d74f42e5e242753eaca3e308af5d8da7039
girvel <widauka@ya.ru>
2024-07-14 23:31:26 +0500

    Group systems by callback in init.lua in order

--- systems/init.lua
+++ systems/init.lua
@@ -1,14 +1,20 @@
 return {
   -- TODO group by callback & period
 
-  -- love.draw systems --
-  require("systems.display"),
-
+  -- for keypressed/mousepressed order is not guaranteed
+  -- love.keypressed systems --
   require("systems.input"),
+
+  -- love.mousepressed systems --
+  require("systems.on_click_calls"),
+
+  -- love.update systems --
   require("systems.acting"),
   require("systems.animation"),
   require("systems.timed_death"),
   require("systems.drift"),
   require("systems.railing"),
-  require("systems.on_click_calls"),
+
+  -- love.draw systems --
+  require("systems.display"),
 }
