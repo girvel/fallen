d95371f9174105d8692521cfb60795b5458162de
girvel <widauka@ya.ru>
2024-07-14 23:27:23 +0500

    Extract hotkeys from player.lua

--- library/player/hotkeys.lua
+++ library/player/hotkeys.lua
@@ -1,11 +1,6 @@
 local actions = require("core.actions")
 local turn_order = require("tech.turn_order")
-local classes = require("core.classes")
-local humanoid = require("core.humanoid")
 
-
-local module_mt = {}
-local module = setmetatable({}, module_mt)
 
 local define_hotkey = function(collection, modes, keys, action)
   for _, m in ipairs(modes) do
@@ -77,50 +72,4 @@ Fun.range(1, 9):each(function(i)
   end)
 end)
 
-module_mt.__call = function()
-  local result = humanoid({
-    player_flag = true,
-    name = "протагонист",
-    class = classes.charming_leader,
-    race = {
-      codename = "player_character",
-      skin_color = Common.hex_color("8ed3dc"),
-    },
-    level = 2,
-    direction = "right",
-    immortal = true,
-    ai = function(self)
-      local mode
-      if State.gui.text_entities then
-        mode = "reading"
-      elseif self.hears then
-        mode = "dialogue"
-      elseif self.dialogue_options then
-        mode = "dialogue_options"
-      elseif State.move_order then
-        mode = "fight"
-      else
-        mode = "free"
-      end
-
-      local action = hotkeys[mode][self.last_pressed_key]
-      self.last_pressed_key = nil
-      if action ~= nil then return action(self) end
-    end,
-    abilities = {
-      strength = 16,
-      dexterity = 18,
-      constitution = 14,
-      intelligence = 8,
-      wisdom = 10,
-      charisma = 8,
-    },
-  })
-
-  result.turn_resources.second_wind = 1
-  result.turn_resources.action_surge = 1
-
-  return result
-end
-
-return module
+return hotkeys
---
+++ library/player/init.lua
@@ -0,0 +1,56 @@
+local classes = require("core.classes")
+local humanoid = require("core.humanoid")
+local hotkeys = require("library.player.hotkeys")
+
+
+local module_mt = {}
+local module = setmetatable({}, module_mt)
+
+
+module_mt.__call = function()
+  local result = humanoid({
+    player_flag = true,
+    name = "протагонист",
+    class = classes.charming_leader,
+    race = {
+      codename = "player_character",
+      skin_color = Common.hex_color("8ed3dc"),
+    },
+    level = 2,
+    direction = "right",
+    immortal = true,
+    ai = function(self)
+      local mode
+      if State.gui.text_entities then
+        mode = "reading"
+      elseif self.hears then
+        mode = "dialogue"
+      elseif self.dialogue_options then
+        mode = "dialogue_options"
+      elseif State.move_order then
+        mode = "fight"
+      else
+        mode = "free"
+      end
+
+      local action = hotkeys[mode][self.last_pressed_key]
+      self.last_pressed_key = nil
+      if action ~= nil then return action(self) end
+    end,
+    abilities = {
+      strength = 16,
+      dexterity = 18,
+      constitution = 14,
+      intelligence = 8,
+      wisdom = 10,
+      charisma = 8,
+    },
+  })
+
+  result.turn_resources.second_wind = 1
+  result.turn_resources.action_surge = 1
+
+  return result
+end
+
+return module
