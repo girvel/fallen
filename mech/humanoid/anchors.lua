aa5cf5fc28b9ec27f7748e7abb53a3885992c115
girvel <widauka@ya.ru>
2024-07-29 15:34:09 +0500

    Half of second attack animations

--- mech/humanoid/anchors.lua
+++ mech/humanoid/anchors.lua
@@ -23,46 +23,6 @@ return {
       other_hand = Vector({3, 12}),
     },
   },
-  main_hand_attack_right = {
-    {
-      main_hand = Vector({2, 12}),
-      other_hand = Vector({10, 12}),
-    },
-    {
-      main_hand = Vector({14, 11}),
-      other_hand = Vector({6, 12}),
-    },
-  },
-  main_hand_attack_left = {
-    {
-      main_hand = Vector({12, 12}),
-      other_hand = Vector({4, 12}),
-    },
-    {
-      main_hand = Vector({0, 11}),
-      other_hand = Vector({8, 12}),
-    },
-  },
-  main_hand_attack_down = {
-    {
-      main_hand = Vector({3, 11}),
-      other_hand = Vector({10, 12}),
-    },
-    {
-      main_hand = Vector({3, 15}),
-      other_hand = Vector({10, 12}),
-    },
-  },
-  main_hand_attack_up = {
-    {
-      main_hand = Vector({11, 13}),
-      other_hand = Vector({4, 12}),
-    },
-    {
-      main_hand = Vector({11, 8}),
-      other_hand = Vector({3, 10}),
-    },
-  },
   move_right = {
     {
       main_hand = Vector({3, 12}),
@@ -117,6 +77,86 @@ return {
     {
       main_hand = Vector({11, 12}),
       other_hand = Vector({3, 12}),
+    },
+  },
+  main_hand_attack_right = {
+    {
+      main_hand = Vector({2, 12}),
+      other_hand = Vector({10, 12}),
+    },
+    {
+      main_hand = Vector({14, 11}),
+      other_hand = Vector({6, 12}),
+    },
+  },
+  main_hand_attack_left = {
+    {
+      main_hand = Vector({12, 12}),
+      other_hand = Vector({4, 12}),
+    },
+    {
+      main_hand = Vector({0, 11}),
+      other_hand = Vector({8, 12}),
+    },
+  },
+  main_hand_attack_down = {
+    {
+      main_hand = Vector({3, 11}),
+      other_hand = Vector({10, 12}),
+    },
+    {
+      main_hand = Vector({3, 15}),
+      other_hand = Vector({10, 12}),
+    },
+  },
+  main_hand_attack_up = {
+    {
+      main_hand = Vector({11, 11}),
+      other_hand = Vector({4, 12}),
+    },
+    {
+      main_hand = Vector({11, 7}),
+      other_hand = Vector({3, 10}),
+    },
+  },
+  other_hand_attack_right = {
+    {
+      main_hand = Vector({2, 12}),
+      other_hand = Vector({10, 12}),
+    },
+    {
+      main_hand = Vector({14, 11}),
+      other_hand = Vector({6, 12}),
+    },
+  },
+  other_hand_attack_left = {
+    {
+      main_hand = Vector({12, 12}),
+      other_hand = Vector({4, 12}),
+    },
+    {
+      main_hand = Vector({0, 11}),
+      other_hand = Vector({8, 12}),
+    },
+  },
+  other_hand_attack_down = {
+    {
+      main_hand = Vector({4, 12}),
+      other_hand = Vector({11, 12}),
+    },
+    {
+      main_hand = Vector({4, 12}),
+      other_hand = Vector({11, 14}),
+    },
+  },
+  other_hand_attack_up = {
+    {
+      main_hand = Vector({10, 12}),
+      other_hand = Vector({3, 11}),
+    },
+    {
+      main_hand = Vector({11, 10}),
+      other_hand = Vector({3, 7}),
     },
   },
   holding_down = {
