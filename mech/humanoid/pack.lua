df6bc79a92ae603ca73bc1d484dda7a2793e1ae4
girvel <widauka@ya.ru>
2024-07-29 14:50:42 +0500

    Atlas-based humanoid animation

--- mech/humanoid/pack.lua
+++ mech/humanoid/pack.lua
@@ -2,4 +2,4 @@ local animated = require("tech.animated")
 local anchors = require("mech.humanoid.anchors")
 
 
-return animated.load_pack("assets/sprites/humanoid", anchors)
+return animated.load_atlas_pack("assets/sprites/humanoid", anchors)
