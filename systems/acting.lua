eefba2f169333ca9ab45995d984c9ae6c86d57ad
girvel <widauka@ya.ru>
2024-08-01 21:54:36 +0500

    FX for player's turn (closes #204)

--- systems/acting.lua
+++ systems/acting.lua
@@ -1,4 +1,5 @@
 local combat = require("tech.combat")
+local fx = require("tech.fx")
 
 
 local your_move_sound = Common.volumed_sounds("assets/sounds/your_move1", 0.5)[1]
@@ -60,21 +61,27 @@ return Tiny.processingSystem({
       entity.ai.run(entity, event) == combat.TURN_END_SIGNAL and not is_world_turn
       or was_timeout_reached
     then
-      Common.reset_period(self, State.combat:get_current())
-      Tablex.extend(entity.resources, -Query(entity):get_resources("move") or {})
-      State.combat:move_to_next()
-      Log.info("%s's turn" % Common.get_name(State.combat:get_current()))
-
-      if State.combat:get_current() == State.player then
-        State.audio:play_static(your_move_sound)
-      end
+      self:_pass_turn()
     end
   end,
 
-  postProcess = function()
-    if -Query(State.combat):get_current() == combat.WORLD_TURN
-    then
-      State.combat:move_to_next()
+  postProcess = function(self)
+    if -Query(State.combat):get_current() == combat.WORLD_TURN then
+      self:_pass_turn()
+    end
+  end,
+
+  _pass_turn = function(self)
+    local current = State.combat:get_current()
+    Common.reset_period(self, current)
+    Tablex.extend(current.resources, -Query(current):get_resources("move") or {})
+    State.combat:move_to_next()
+    current = State.combat:get_current()
+    Log.info("%s's turn" % Common.get_name(current))
+
+    if current == State.player then
+      State.audio:play_static(your_move_sound)
+      State:add(fx("assets/sprites/fx/turn_starts", "fx_behind", current.position))
     end
   end,
 })
