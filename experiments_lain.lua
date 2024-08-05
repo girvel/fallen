a01fbec3025c1f0f3424ee5a0012d7844a276b06
girvel <widauka@ya.ru>
2024-08-05 18:35:53 +0500

    libtcod + LuaJIT FFI proof of concept

--- experiments_lain.lua
+++ experiments_lain.lua
@@ -6,10 +6,40 @@ local experiments = {}
 
 experiments.tcod = function()
   ffi.cdef([[
+    typedef enum {
+      FOV_BASIC, 
+      FOV_DIAMOND, 
+      FOV_SHADOW, 
+      FOV_PERMISSIVE_0,FOV_PERMISSIVE_1,FOV_PERMISSIVE_2,FOV_PERMISSIVE_3,
+      FOV_PERMISSIVE_4,FOV_PERMISSIVE_5,FOV_PERMISSIVE_6,FOV_PERMISSIVE_7,FOV_PERMISSIVE_8, 
+      FOV_RESTRICTIVE,
+      NB_FOV_ALGORITHMS
+    } TCOD_fov_algorithm_t;
+
     struct TCOD_Map *TCOD_map_new(int width, int height);
+
+    void TCOD_map_set_properties(
+      struct TCOD_Map *map, int x, int y, bool is_transparent, bool is_walkable
+    );
+
+    void TCOD_map_compute_fov(
+      struct TCOD_Map *map, int player_x, int player_y, int max_radius, bool light_walls,
+      TCOD_fov_algorithm_t algo
+    );
+
+    bool TCOD_map_is_transparent(struct TCOD_Map *map, int x, int y);
+    bool TCOD_map_is_walkable(struct TCOD_Map *map, int x, int y);
+    bool TCOD_map_is_in_fov(struct TCOD_Map *map, int x, int y);
   ]])
   local libtcod = ffi.load("lib/libtcod")
-  libtcod.TCOD_map_new(3, 3)
+
+  local map = libtcod.TCOD_map_new(3, 3)
+  libtcod.TCOD_map_set_properties(map, 1, 1, false, true)
+  Log.info(libtcod.TCOD_map_is_transparent(map, 1, 1))
+  Log.info(libtcod.TCOD_map_is_walkable(map, 1, 1))
+  libtcod.TCOD_map_compute_fov(map, 0, 0, 3, true, ffi.C.FOV_BASIC)
+  Log.info(libtcod.TCOD_map_is_in_fov(map, 1, 1))
+  Log.info(libtcod.TCOD_map_is_in_fov(map, 2, 2))
 end
 
 experiments.serialization = function()
