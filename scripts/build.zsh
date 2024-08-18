c2bb62aec2909b8d6413cc64bdab0748c185b536
girvel <widauka@ya.ru>
2024-08-18 05:07:19 +0500

    Update build script

--- scripts/build.zsh
+++ scripts/build.zsh
@@ -1,17 +1,16 @@
 #!/bin/env zsh
 mkdir -p .build
+rm -rf .build/*
 
 zip -9 -r .build/fallen.love assets lib library mech state systems tech utils conf.lua main.lua
 
-rm -f .build/fallen.exe
 cat "$(which love.exe)" .build/fallen.love > .build/fallen.exe
 rm .build/fallen.love
 
-rm -rf .build/fallen
 mkdir -p .build/fallen
 mv .build/fallen.exe .build/fallen/fallen.exe
 cp "$(dirname "$(which love.exe)")"/*.dll .build/fallen/
 cp lib/*.dll .build/fallen/
-cd .build; zip -9 -r fallen.zip fallen; cd ..
+cd .build; zip -9 -r fallen_win64.zip fallen; cd ..
 
 rm -rf .build/fallen
