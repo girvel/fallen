62ae242ae3c4a4adc06f0aca899ea2a1853a827a
girvel <widauka@ya.ru>
2024-09-01 16:39:01 +0500

    #

--- lib/dump.lua
+++ lib/dump.lua
@@ -1,3 +1,7 @@
+local module_mt = {}
+local dump = setmetatable({}, module_mt)
+
+
 local to_expression = function(statement)
   return ("(function()\n%s\nend)()"):format(statement)
 end
@@ -6,41 +10,67 @@ local handle_primitive
 local stack, warnings
 local allowed_big_upvalues = {}
 
-local build_table = function(x, cache)
-  local mt = getmetatable(x)
-  if mt and mt.__serialize then
-    local serialized = mt.__serialize(x)
-    if type(serialized) == "function" then
-      allowed_big_upvalues[serialized] = true
-      return ("return %s()"):format(handle_primitive(serialized, cache))
-    end
-    if type(serialized) == "string" then
-      return "return " .. serialized
-    end
-  end
 
+dump.get_warnings = function() return {unpack(warnings)} end
+dump.ignore_upvalue_size = setmetatable({}, {
+  __concat = function(self, other)
+    return self(other)
+  end,
+  __call = function(self, other)
+    allowed_big_upvalues[other] = true
+    return other
+  end,
+})
+dump.require_path = nil
+dump.serializers = {}
+
+
+local build_table = function(x, cache)
   cache.size = cache.size + 1
   cache[x] = cache.size
 
+  local mt = getmetatable(x)
+
+  local serialized
+  if dump.serializers[x] then
+    serialized = dump.serializers[x](x)
+  elseif mt and mt.__serialize then
+    serialized = mt.__serialize(x)
+  end
+
   local result = {}
-  result[1] = "local _ = {}"
-  result[2] = ("cache[%s] = _"):format(cache.size)
-
-  for k, v in pairs(x) do
-    table.insert(stack, tostring(k))
-    table.insert(result, ("_[%s] = %s"):format(
-      handle_primitive(k, cache),
-      handle_primitive(v, cache)
-    ))
-    table.remove(stack)
-  end
-
-  if not mt then
-    table.insert(result, "return _")
+  if serialized then
+    if type(serialized) == "function" then
+      allowed_big_upvalues[serialized] = true
+      table.insert(result, ("local _ = %s()"):format(handle_primitive(serialized, cache)))
+    end
+    if type(serialized) == "string" then
+      table.insert(result, "local _ = " .. serialized)
+    end
   else
-    table.insert(result, ("return setmetatable(_, %s)"):format(handle_primitive(mt, cache)))
+    table.insert(result, "local _ = {}")
+    table.insert(result, ("cache[%s] = _"):format(cache.size))
+    if mt then
+      table.insert(result, ("setmetatable(_, %s)"):format(handle_primitive(mt, cache)))
+    end
+
+    for k, v in pairs(x) do
+      table.insert(stack, tostring(k))
+      table.insert(result, ("_[%s] = %s"):format(
+        handle_primitive(k, cache),
+        handle_primitive(v, cache)
+      ))
+      table.remove(stack)
+    end
   end
 
+  if dump.serializers[x] then
+    table.insert(result, ("dump.serializers[_] = %s"):format(
+      handle_primitive(dump.serializers[x], cache)
+    ))
+  end
+
+  table.insert(result, "return _")
   return table.concat(result, "\n")
 end
 
@@ -115,36 +145,23 @@ handle_primitive = function(x, cache)
   return primitives[xtype](x, cache)
 end
 
-return setmetatable({
-  get_warnings = function() return {unpack(warnings)} end,
-  ignore_upvalue_size = setmetatable({}, {
-    __concat = function(self, other)
-      return self(other)
-    end,
-    __call = function(self, other)
-      allowed_big_upvalues[other] = true
-      return other
-    end,
-  }),
-  require_path = nil,
-  serializers = {},
-}, {
-  __call = function(self, x)
-    assert(
-      self.require_path,
-      "Put the lua path to dump libary into dump.require_path before calling dump itself"
-    )
+module_mt.__call = function(self, x)
+  assert(
+    self.require_path,
+    "Put the lua path to dump libary into dump.require_path before calling dump itself"
+  )
 
-    stack = {}
-    warnings = {}
-    local cache = {size = 0}
-    local result
-    if type(x) == "table" then
-      result = build_table(x, cache)
-    else
-      result = "return " .. handle_primitive(x, cache)
-    end
-
-    return ("local cache = {}\nlocal dump = require(\"%s\")\n"):format(self.require_path) .. result
+  stack = {}
+  warnings = {}
+  local cache = {size = 0}
+  local result
+  if type(x) == "table" then
+    result = build_table(x, cache)
+  else
+    result = "return " .. handle_primitive(x, cache)
   end
-})
+
+  return ("local cache = {}\nlocal dump = require(\"%s\")\n"):format(self.require_path) .. result
+end
+
+return dump
