69c5d2b6b96b442daae50a0cc3485fe3ecbb0f44
girvel <widauka@ya.ru>
2024-09-03 10:00:21 +0500

    math.random() ditribution test script

---
+++ scripts/random/main.lua
@@ -0,0 +1,16 @@
+local fun = require("lib.fun")
+
+
+local N = 100000
+local points = fun.range(N)
+  :map(function(i) return {
+    math.random() * love.graphics.getWidth(),
+    math.random() * love.graphics.getHeight(),
+    i / N, i / N, 1, 1,
+  } end)
+  :totable()
+
+love.draw = function()
+  love.graphics.setColor({1, 1, 1})
+  love.graphics.points(points)
+end
