4618b45dccdd1c6660952faf2e2c2f7706bfd209
girvel <widauka@ya.ru>
2024-09-04 10:00:28 +0500

    Disable perspective for on-wall on_solids

--- library/palette/on_solids.lua
+++ library/palette/on_solids.lua
@@ -3,16 +3,23 @@ local factoring = require("tech.factoring")
 
 local on_solids, module_mt, static = Module("library.palette.on_solids")
 
-factoring.from_atlas(on_solids, "assets/sprites/atlases/on_solids.png", {
-  layer = "on_solids",
-  view = "scene",
-  perspective_flag = true,
-}, {
-  "mirror", "dirt", "airway", "map", "sign", "window", false, "cauldron",
-  "vines", "vines", "vines", "vines", "vines", false, false, "cauldron",
-  false, "vines", "vines", "vines", "vines", false, false, false,
-  false, false, false, "vines", "vines", false, false, false,
-  false, false, false, "vines", "vines", false, false, false,
+local use_perspective = Common.set({
+  "mirror", "airway", "map", "sign", "cauldron"
 })
+
+factoring.from_atlas(on_solids, "assets/sprites/atlases/on_solids.png",
+  function(name) return {
+    layer = "on_solids",
+    view = "scene",
+    perspective_flag = use_perspective[name],
+  } end,
+  {
+    "mirror", "dirt", "airway", "map", "sign", "window", false, "cauldron",
+    "vines", "vines", "vines", "vines", "vines", false, false, "cauldron",
+    false, "vines", "vines", "vines", "vines", false, false, false,
+    false, false, false, "vines", "vines", false, false, false,
+    false, false, false, "vines", "vines", false, false, false,
+  }
+)
 
 return on_solids
