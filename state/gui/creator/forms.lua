004c4916a065b5f1eb5ce8f9aec0e1b7598b3f7e
girvel <widauka@ya.ru>
2024-09-14 14:17:03 +0500

    Fix alignment

--- state/gui/creator/forms.lua
+++ state/gui/creator/forms.lua
@@ -7,9 +7,9 @@ local forms, module_mt, static = Module("state.gui.creator.forms")
 forms.class = static .. function(mixin)
   return Html(function()
     return span {
-      h2 {"%s%s" % {translation.class[mixin.class.codename], mixin.level}},
+      "   ", h2 {"Класс: %s, уровень %s" % {translation.class[mixin.class.codename], mixin.level}},
       unpack(Fun.iter(experience.get_progression(mixin.class, mixin.level))
-        :map(function(perk) return p {Entity.name(perk)} end)
+        :map(function(perk) return p {"   ", Entity.name(perk)} end)
         :totable()
       ),
     }
