85761e16219ef2eb165d2b87c97b40734e2c63f4
girvel <widauka@ya.ru>
2024-09-17 12:00:12 +0500

    Plan HTML rework via tests

--- lib/tests/test_html.lua
+++ lib/tests/test_html.lua
@@ -20,31 +20,103 @@ describe("Html generator", function()
 
     return fn
   end
-  _G.unpack = table.unpack
-  _G.Table = require("lib.extensions.table")
-  _G.Pairs = require("lib.extensions.pairs")
-  _G.Module = require("lib.types.module")
-  _G.OrderedMap = require("lib.types.ordered_map")
 
-  it("works", function()
+  -- _G.Table = require("lib.extensions.table")
+  -- _G.Pairs = require("lib.extensions.pairs")
+  -- _G.Module = require("lib.types.module")
+  -- _G.OrderedMap = require("lib.types.ordered_map")
+
+  it("generates an internal representation tree", function()
+    local tag = html().tag
     assert.equal(
-      '<div class="someclass"><h1>ABC</h1><p><span>A</span>BC</p></div>',
-      html(function()
-        return div {
+      {
+        __type = tag,
+        tag = "div",
+        attributes = {
           class = "someclass",
-          h1 {"ABC"},
-          p {span {"A"}, "BC"},
-        }
-      end)
+        },
+        content = {
+          {
+            __type = tag,
+            tag = "h1",
+            attributes = {},
+            content = {"ABC"},
+          },
+          {
+            __type = tag,
+            tag = "p",
+            attributes = {},
+            content = {
+              {
+                __type = tag,
+                tag = "span",
+                attributes = {},
+                content = {"A"},
+              },
+              "BC",
+            },
+          },
+        },
+      },
+      html.div {
+        class = "someclass",
+        html.h1 {"ABC"},
+        html.p {html.span{"A"}, "BC"},
+      }
     )
   end)
 
-  it("handles any types", function()
+  it("can handle attributes of any type, but the content is converted to string", function()
     local hello = setmetatable({}, {__tostring = function() return "Hello" end})
-    assert.equal("<div>Hello</div>", html(function() return div {hello} end))
+    local f = function() end
+    assert.same(
+      {
+        __type = html().tag,
+        tag = "div",
+        attributes = {
+          integer = 1,
+          boolean = true,
+          fn = f,
+          table = hello,
+        },
+        content = {
+          "Hello",
+          "based",
+        },
+      },
+      html.div {
+        class = "based",
+        integer = 1,
+        boolean = true,
+        fn = f,
+        table = hello,
+        hello,
+        "based",
+      }
+    )
   end)
 
-  it("handles short tags", function()
-    assert.equal("<br/>", html(function() return br() end))
+  it("can parse HTML", function()
+    local tag = html().tag
+    assert.same(
+      {
+        __type = tag,
+        tag = "div",
+        attributes = {class = "a"},
+        content = {
+          "Hello, ",
+          {
+            __type = tag,
+            tag = "span",
+            attributes = {},
+            content = {"world"},
+          },
+          "!",
+        },
+      },
+      html().parse('<div class="a">Hello, <span>world</span>!</div>')
+    )
   end)
+
+  -- TODO! it can convert attribute types
 end)
