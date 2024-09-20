9c8cc2a116a88e913d99f951fe4d97c4f1ca849b
Nikita Dobrynin <widauka@ya.ru>
2024-09-20 13:28:15 +0500

    Transition existing creator rewrite to new texting

--- state/gui/creator/forms.lua
+++ state/gui/creator/forms.lua
@@ -5,24 +5,23 @@ local experience = require("mech.experience")
 local forms, module_mt, static = Module("state.gui.creator.forms")
 
 local perk_to_html = function(perk)
-  return Html(function()
-    return p {
-      "   ",
-      span {tooltip = perk.codename, Entity.name(perk)},
-    }
-  end)
+  return Html.p {
+    "   ",
+    Html.span {tooltip = perk.codename, Entity.name(perk)},
+  }
 end
 
 forms.class = static .. function(mixin)
-  return Html(function()
-    return span {
-      "   ", h2 {"Класс: %s, уровень %s" % {translation.class[mixin.class.codename], mixin.level}},
-      unpack(Fun.iter(experience.get_progression(mixin.class, mixin.level))
-        :map(perk_to_html)
-        :totable()
-      ),
-    }
-  end)
+  return Html.span {
+    "   ",
+    Html.h2 {"Класс: %s, уровень %s" % {
+      translation.class[mixin.class.codename], mixin.level
+    }},
+    unpack(Fun.iter(experience.get_progression(mixin.class, mixin.level))
+      :map(perk_to_html)
+      :totable()
+    ),
+  }
 end
 
 return forms
