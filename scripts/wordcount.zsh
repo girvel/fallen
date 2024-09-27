68143ae833b28aeb951d0666033bc4189e71793c
Nikita Dobrynin <widauka@ya.ru>
2024-09-27 13:20:58 +0500

    Fix wordcount script

--- scripts/wordcount.zsh
+++ scripts/wordcount.zsh
@@ -1,4 +1,4 @@
 #!/bin/env zsh
 a=$(cat **/*.lua | wc -l)
-cd lib; b=$(cat lust.lua profile.lua argparse.lua inspect.lua fun.lua memoize.lua strong.lua tiny.lua log.lua htmlparser/*.lua | wc -l)
+b=$(cat lib/vendor/**/*.lua | wc -l)
 echo $((a - b))
