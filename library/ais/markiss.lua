e3769e66b751455e68adb8218a264fadbf080b6a
girvel <widauka@ya.ru>
2024-10-16 16:14:34 +0500

    Markiss avoiding moving obstacles

--- library/ais/markiss.lua
+++ library/ais/markiss.lua
@@ -41,6 +41,7 @@ module_mt.__call = function(_, travel_points)
 
         if not self._last_path then
           self._last_path = tcod.snapshot():find_path(entity.position, self._travel_points[1])
+          Log.trace(entity.position, self._last_path)
         end
 
         if self._next_path_i > #self._last_path then
@@ -48,6 +49,9 @@ module_mt.__call = function(_, travel_points)
         end
 
         if not api.move(entity, self._last_path[self._next_path_i] - entity.position) then
+          self._last_path = nil
+          self._next_path_i = 1
+          self._mode = markiss.mode.paused(1)
           return
         end
         self._next_path_i = self._next_path_i + 1
