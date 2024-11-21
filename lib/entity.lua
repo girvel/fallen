51bef32add74ed7ccf0099439d649b093ee5f394
girvel <widauka@ya.ru>
2024-11-21 14:18:30 +0500

    Remove docs/definitions/

--- lib/entity.lua
+++ lib/entity.lua
@@ -1,3 +1,30 @@
+--- @class entity
+--- Abstract class for all in-game ECS entities.
+--- Contains all possible public fields.
+---
+--- @field name? string name to be displayed in-game
+--- @field codename? string name to be displayed/used as an index in-code
+---
+--- @field position? vector in-game position in relation to `.view`
+--- @field size? vector size (in `.view`)
+--- @field view? string coordinate system's index in `State.gui.views`
+--- @field layer? layer layer in `State.grids`
+---
+--- @field sprite? sprite
+--- @field shader? shader shader used specifically for this entity; surpasses State's shader
+---
+--- @field hp? integer current amount of health points; hp <= 0 means death
+--- @field get_max_hp? fun(self: entity): integer calculate maximal possible amount of HP
+---
+--- @field inventory? {[string]: table} single-item displayable inventory slots
+---
+--- @field ai? ai
+---
+--- @field boring_flag? true disable logging `State:remove`
+--- @field creature_flag? true entity was created as mech.creature
+---
+--- @field last_initiative? number value of the last initiative roll
+
 local entityx = {}
 
 entityx.is_over = function(position, entity)
@@ -9,3 +36,6 @@ entityx.name = function(entity)
 end
 
 return entityx
+
+-- TODO maybe better tech.entity because the module is not exactly an independent library?
+--   moreover it kind of describes the main architectural feature of Fallen: in-game entities.
