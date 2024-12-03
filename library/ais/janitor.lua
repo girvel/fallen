6b8dfaeea650f0549d14fd6c0493e59ed4655372
girvel <widauka@ya.ru>
2024-12-03 16:50:22 +0500

    Janitor -- react to messing w/ his bucket

--- library/ais/janitor.lua
+++ library/ais/janitor.lua
@@ -3,6 +3,7 @@ local tcod = require("tech.tcod")
 local ai = require("tech.ai")
 local decorations = require("library.palette.decorations")
 local items       = require("library.palette.items")
+local railing     = require("tech.railing")
 local api = ai.api
 
 
@@ -12,32 +13,47 @@ local TRAVEL_R = 8
 
 module_mt.__call = function(_)
   return {
-    run = ai.async(function(self, entity)
-      local path
-      while true do
-        local destination = entity.position + Vector {
-          math.random(-TRAVEL_R, TRAVEL_R),
-          math.random(-TRAVEL_R, TRAVEL_R),
-        }
+    _current_line = 0,
+    _lines = {
+      "Ой.",
+      "Опять пролилась.",
+      "Пожалуйста, не делай этого.",
+      "Ты это специально?",
+      "Ещё раз и у тебя будут проблемы.",
+      "Ну всё, я тебе покажу!",
+    },
+    _popup = {},
 
-        path = tcod.snapshot():find_path(entity.position, destination)
-        if #path > 2 then break end
+    run = ai.async(function(self, entity)
+      -- 1. go to a new place --
+      do
+        local path
+        while true do
+          local destination = entity.position + Vector {
+            math.random(-TRAVEL_R, TRAVEL_R),
+            math.random(-TRAVEL_R, TRAVEL_R),
+          }
+
+          path = tcod.snapshot():find_path(entity.position, destination)
+          if #path > 2 then break end
+        end
+
+        api.follow_path(entity, path)
       end
 
-      api.follow_path(entity, path)
-
-      local bucket_position, rotation
+      -- 2. place the bucket --
+      local bucket_position, bucket_direction_name
       for _, direction_name in ipairs(Vector.direction_names) do
         bucket_position = entity.position + Vector[direction_name]
         if not State.grids.solids[bucket_position] then
-          rotation = direction_name
+          bucket_direction_name = direction_name
           goto position_found
         end
       end
       do return end
 
       ::position_found::
-      entity:rotate(rotation)
+      entity:rotate(bucket_direction_name)
       coroutine.yield()
 
       State:remove(entity.inventory.other_hand)
@@ -45,12 +61,37 @@ module_mt.__call = function(_)
       State:add(decorations.bucket(), {position = bucket_position})
       coroutine.yield()
 
-      entity:rotate(Random.choice({"up", "down"}))
+      -- 3. mop the floor --
+      local washing_direction_name = Random.choice({"up", "down"})
+      entity:rotate(washing_direction_name)
       for _ = 1, math.random(5, 10) do
         entity:animate("main_hand_attack")
         api.wait_seconds(.8)
+
+        local current_bucket = State.grids.solids[bucket_position]
+        if current_bucket.codename == "bucketd" then
+          entity:rotate(bucket_direction_name)
+          coroutine.yield()
+
+          self._current_line = self._current_line + 1
+          if State:exists(self._popup[1]) then
+            State:remove_multiple(self._popup)
+          end
+          self._popup = railing.api.message.temporal(self._lines[self._current_line], {source = entity})
+          -- TODO! make agressive here
+          coroutine.yield()
+
+          current_bucket.on_remove = function()
+            State:add(decorations.bucket(), {position = current_bucket.position})
+          end
+          State:remove(current_bucket)
+          coroutine.yield()
+
+          entity:rotate(washing_direction_name)
+        end
       end
 
+      -- 4. pick up the bucket --
       local e = State.grids.solids[bucket_position]
       if e and (e.codename == "bucket" or e.codename == "bucketd") then
         e.on_remove = nil
