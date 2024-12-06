9f5670470f3adbf9200f11be10df03ca9e37aba8
girvel <widauka@ya.ru>
2024-12-06 17:18:02 +0500

    Annotate lib.string

--- lib/string.lua
+++ lib/string.lua
@@ -1,6 +1,11 @@
 local utf8 = require("utf8")
 
 
+--- Get a UTF-8-compatible substring
+--- @param str string
+--- @param a integer
+--- @param b? integer defaults to the length of the string
+--- @return string
 string.utf_sub = function(str, a, b)
   if not b then
     b = string.utf_len(str)
@@ -10,6 +15,9 @@ string.utf_sub = function(str, a, b)
   return str:sub(utf8.offset(str, a), utf8.offset(str, b + 1) - 1)
 end
 
+--- Get UTF-8-compatible length of the string
+--- @param str string
+--- @return integer
 string.utf_len = function(str)
   return utf8.len(str)
 end
@@ -17,6 +25,9 @@ end
 local ru_lower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 local ru_upper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
 
+--- Put all UTF-8 characters of `str` into lowercase
+--- @param str string
+--- @return string
 string.utf_lower = function(str)
   str = str:lower()
   for i = 1, string.utf_len(ru_lower) do
@@ -28,6 +39,9 @@ string.utf_lower = function(str)
   return str
 end
 
+--- Put all UTF-8 characters of `str` into uppercase
+--- @param str string
+--- @return string
 string.utf_upper = function(str)
   str = str:upper()
   for i = 1, string.utf_len(ru_lower) do
@@ -39,14 +53,24 @@ string.utf_upper = function(str)
   return str
 end
 
+--- @param str string
+--- @param prefix string
+--- @return boolean
 string.starts_with = function(str, prefix)
   return str:sub(1, #prefix) == prefix
 end
 
-string.ends_with = function(str, suffix)
-  return str:sub(-#suffix, -1) == suffix
+--- @param str string
+--- @param postfix string
+--- @return boolean
+string.ends_with = function(str, postfix)
+  return str:sub(-#postfix, -1) == postfix
 end
 
+--- @param str string
+--- @param pat string
+--- @param plain boolean?
+--- @return string[]
 string.split = function(str, pat, plain)
   local t = {}
 
@@ -63,6 +87,10 @@ string.split = function(str, pat, plain)
   end
 end
 
+--- @param str string
+--- @param int integer
+--- @param padstr string
+--- @return string
 string.ljust = function(str, int, padstr)
   local len = utf8.len(str)
 
@@ -76,6 +104,10 @@ string.ljust = function(str, int, padstr)
   return str
 end
 
+--- @param str string
+--- @param int integer
+--- @param padstr string
+--- @return string
 string.rjust = function(str, int, padstr)
   local len = #str
 
@@ -89,18 +121,26 @@ string.rjust = function(str, int, padstr)
   return str
 end
 
+--- @param str string
+--- @return string
 string.lstrip = function(str)
-  return str:gsub("^%s+", "")
+  return select(1, str:gsub("^%s+", ""))
 end
 
+--- @param str string
+--- @return string
 string.rstrip = function(str)
-  return str:gsub("%s+$", "")
+  return select(1, str:gsub("%s+$", ""))
 end
 
+--- @param str string
+--- @return string
 string.strip = function(str)
   return string.rstrip(string.lstrip(str))
 end
 
+--- @param str string
+--- @return string
 string.indent = function(str)
   return table.concat(Fun.iter(str / "\n")
     :map(function(line) return "  " .. line end)
